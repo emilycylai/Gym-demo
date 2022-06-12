f817eabe0b0d23c1f9492935460d7e90335d1205
Emily <lai810717@gmail.com>
2022-06-12 12:19:12 +0800

    BMI計算完成

--- js/bmi.js
+++ js/bmi.js
@@ -2,6 +2,8 @@ function getBmi(height,weight,point =2){
     return (weight/(height/100)**2).toFixed(point);
 }
 function calcBmi(){
+    const bmiResult = document.querySelector(".bmi");
+    const bmiComment = document.querySelector(".comment");
     let name = nameEl.value;
     let height = heightEl.value;
     let weight = weightEl.value;
@@ -9,9 +11,19 @@ function calcBmi(){
     let bmi= getBmi(height,weight);
 
     if (isNaN(bmi)){
-        alert("輸入錯誤");
+        bmiResult.innerText = "輸入錯誤！";
         return;
     }
+
+    bmiResult.innerText = bmi;
+    if (bmi <18.5){
+        bmiComment.innerText="太輕了！";
+    } else if (bmi >=18.5 && bmi<24){
+        bmiComment.innerText="標準！";
+    } else {
+        bmiComment.innerText="肥胖！";
+    }
+    
 
     console.log(name,height,weight,bmi);
 
